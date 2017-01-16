5d46eb1f625baa7bba6d85e648982e9bd457f236
wly1995 <550612304@qq.com>
2017-01-16 16:29:20 +0800

    第十一次提交，导入PhotoView库，完成图片的动图的调转展示

--- app/src/main/java/com/atguigu/mobiletest/ShowImageAndGifActivity.java
+++ app/src/main/java/com/atguigu/mobiletest/ShowImageAndGifActivity.java
@@ -1,13 +1,63 @@
 package com.atguigu.mobiletest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import org.xutils.common.Callback;
+import org.xutils.common.util.DensityUtil;
+import org.xutils.image.ImageOptions;
+import org.xutils.x;
+
+import uk.co.senab.photoview.PhotoView;
+import uk.co.senab.photoview.PhotoViewAttacher;
 
 public class ShowImageAndGifActivity extends AppCompatActivity {
-
+    private String url;
+    private ImageOptions imageOptions;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_image_and_gif);
+        //得到数据
+        url = getIntent().getStringExtra("url");
+        PhotoView photoView = (PhotoView) findViewById(R.id.iv_photo);
+
+        final PhotoViewAttacher attacher = new PhotoViewAttacher(photoView);
+
+        //设置xUtils3的配置
+        imageOptions = new ImageOptions.Builder()
+//                .setSize(DensityUtil.dip2px(80), DensityUtil.dip2px(80))
+                //设置圆角
+                .setRadius(DensityUtil.dip2px(5))
+                .setIgnoreGif(false)//是否忽略gif图。false表示不忽略。不写这句，默认是true
+                .setImageScaleType(ImageView.ScaleType.CENTER_CROP)
+                .setLoadingDrawableId(R.drawable.video_default)
+                .setFailureDrawableId(R.drawable.video_default)
+                .build();
+        //用Xutil3进行设置图片或者动图
+        x.image().bind(photoView, url, imageOptions, new Callback.CommonCallback<Drawable>() {
+            @Override
+            public void onSuccess(Drawable result) {
+                //成功后要设置更新
+                attacher.update();
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+
+            }
+
+            @Override
+            public void onFinished() {
+
+            }
+        });
     }
 }
