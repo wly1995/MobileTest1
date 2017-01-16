fd98a019f3b5c98752af55a55453029929b9d262
wly1995 <550612304@qq.com>
2017-01-16 15:18:20 +0800

    第六次提交，完成videoHolder的数据绑定和设置

--- app/src/main/java/com/atguigu/mobiletest/adapter/NetAudioFragmentAdapter.java
+++ app/src/main/java/com/atguigu/mobiletest/adapter/NetAudioFragmentAdapter.java
@@ -5,11 +5,21 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.atguigu.mobiletest.R;
 import com.atguigu.mobiletest.bean.NetAudioBean;
+import com.atguigu.mobiletest.util.Utils;
+import com.bumptech.glide.Glide;
+
+import org.xutils.x;
 
 import java.util.List;
+
+import fm.jiecao.jcvideoplayer_lib.JCVideoPlayer;
+import fm.jiecao.jcvideoplayer_lib.JCVideoPlayerStandard;
 
 import static android.content.ContentValues.TAG;
 
@@ -107,7 +117,7 @@ public class NetAudioFragmentAdapter extends BaseAdapter {
         return convertView;
     }
 
-    private View initView(View convertView, int itemViewType, NetAudioBean.ListBean listBean) {
+    private View initView(View convertView, int itemViewType, NetAudioBean.ListBean mediaItem) {
         switch (itemViewType) {
             case TYPE_VIDEO://视频
 
@@ -180,5 +190,116 @@ public class NetAudioFragmentAdapter extends BaseAdapter {
                 break;
         }
         return convertView;
+    }
+
+
+    class VideoHoder extends BaseViewHolder {
+        Utils utils;
+        TextView tvContext;
+        JCVideoPlayerStandard jcvVideoplayer;
+        TextView tvPlayNums;
+        TextView tvVideoDuration;
+        ImageView ivCommant;
+        TextView tvCommantContext;
+
+        VideoHoder(View convertView) {
+            //这个方法一定要写
+            super(convertView);
+            //中间公共部分 -所有的都有
+            tvContext = (TextView) convertView.findViewById(R.id.tv_context);
+            utils = new Utils();
+            tvPlayNums = (TextView) convertView.findViewById(R.id.tv_play_nums);
+            tvVideoDuration = (TextView) convertView.findViewById(R.id.tv_video_duration);
+            ivCommant = (ImageView) convertView.findViewById(R.id.iv_commant);
+            tvCommantContext = (TextView) convertView.findViewById(R.id.tv_commant_context);
+            jcvVideoplayer = (JCVideoPlayerStandard) convertView.findViewById(R.id.jcv_videoplayer);
+        }
+        /**
+         * 绑定数据
+         *
+         * @param mediaItem
+         */
+        public void setData(NetAudioBean.ListBean mediaItem) {
+            super.setData(mediaItem);
+
+            //设置文本-所有的都有,只有广告没有哦
+            tvContext.setText(mediaItem.getText() + "_" + mediaItem.getType());
+
+            //视频特有的------------------------
+            //第一个参数是视频播放地址，第二个参数是显示封面的地址，第三参数是标题
+            boolean setUp = jcvVideoplayer.setUp(
+                    mediaItem.getVideo().getVideo().get(0), JCVideoPlayer.SCREEN_LAYOUT_LIST,
+                    "");
+            //加载图片
+            if (setUp) {
+//                ImageLoader.getInstance().displayImage(mediaItem.getVideo().getThumbnail().get(0),
+//                        jcvVideoplayer.thumbImageView);
+                Glide.with(mContext).load(mediaItem.getVideo().getThumbnail().get(0)).into(jcvVideoplayer.thumbImageView);
+            }
+            tvPlayNums.setText(mediaItem.getVideo().getPlaycount() + "次播放");
+            tvVideoDuration.setText(utils.stringForTime(mediaItem.getVideo().getDuration() * 1000) + "");
+        }
+    }
+    class BaseViewHolder {
+
+        ImageView ivHeadpic;
+        TextView tvName;
+        TextView tvTimeRefresh;
+        ImageView ivRightMore;
+        ImageView ivVideoKind;
+        TextView tvVideoKindText;
+        TextView tvShenheDingNumber;
+        TextView tvShenheCaiNumber;
+        TextView tvPostsNumber;
+        LinearLayout llDownload;
+
+        public BaseViewHolder(View convertView) {
+            //公共的-头部
+            ivHeadpic = (ImageView) convertView.findViewById(R.id.iv_headpic);
+            tvName = (TextView) convertView.findViewById(R.id.tv_name);
+            tvTimeRefresh = (TextView) convertView.findViewById(R.id.tv_time_refresh);
+            ivRightMore = (ImageView) convertView.findViewById(R.id.iv_right_more);
+            //公共部分-bottom
+            ivVideoKind = (ImageView) convertView.findViewById(R.id.iv_video_kind);
+            tvVideoKindText = (TextView) convertView.findViewById(R.id.tv_video_kind_text);
+            tvShenheDingNumber = (TextView) convertView.findViewById(R.id.tv_shenhe_ding_number);
+            tvShenheCaiNumber = (TextView) convertView.findViewById(R.id.tv_shenhe_cai_number);
+            tvPostsNumber = (TextView) convertView.findViewById(R.id.tv_posts_number);
+            llDownload = (LinearLayout) convertView.findViewById(R.id.ll_download);
+        }
+
+        public void setData(NetAudioBean.ListBean mediaItem) {
+            if (mediaItem.getU() != null && mediaItem.getU().getHeader() != null && mediaItem.getU().getHeader().get(0) != null) {
+                x.image().bind(ivHeadpic, mediaItem.getU().getHeader().get(0));
+            }
+            if (mediaItem.getU() != null && mediaItem.getU().getName() != null) {
+                tvName.setText(mediaItem.getU().getName() + "");
+            }
+
+            tvTimeRefresh.setText(mediaItem.getPasstime());
+
+            //设置标签
+            List<NetAudioBean.ListBean.TagsEntity> tagsEntities = mediaItem.getTags();
+            if (tagsEntities != null && tagsEntities.size() > 0) {
+                StringBuffer buffer = new StringBuffer();
+                for (int i = 0; i < tagsEntities.size(); i++) {
+                    buffer.append(tagsEntities.get(i).getName() + " ");
+                }
+                tvVideoKindText.setText(buffer.toString());
+            }
+
+            //设置点赞，踩,转发
+
+            tvShenheDingNumber.setText(mediaItem.getUp());
+            tvShenheCaiNumber.setText(mediaItem.getDown() + "");
+            tvPostsNumber.setText(mediaItem.getForward() + "");
+
+        }
+    }
+    class ImageHolder extends BaseViewHolder{
+
+        public ImageHolder(View convertView) {
+            super(convertView);
+        }
     }
 }
